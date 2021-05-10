88ebb30306fa11785db6778aaa2146ad789fa7fe
624990742 <624990742@qq.com>
2021-05-10 14:29:10 +0800

    添加了一些文件

---
+++ JCMaNongChenZaiTest/Classes/JCTestManagerTool.h
@@ -0,0 +1,16 @@
+//
+//  JCTestManagerTool.h
+//  Pods
+//
+//  Created by jiachen on 2021/5/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JCTestManagerTool : NSObject
++(NSString *)testMethond;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JCMaNongChenZaiTest/Classes/JCTestManagerTool.m
@@ -0,0 +1,14 @@
+//
+//  JCTestManagerTool.m
+//  Pods
+//
+//  Created by jiachen on 2021/5/10.
+//
+
+#import "JCTestManagerTool.h"
+
+@implementation JCTestManagerTool
++(NSString *)testMethond{
+   return @"码农晨仔";
+}
+@end
